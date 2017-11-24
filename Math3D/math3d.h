268d9e0a28b4b8d04b05cf5f43eec0826d1969ef
hagen23 <hagennav@gmail.com>
2017-11-24 13:06:49 -0600

    Cleaning the repo

--- Math3D/math3d.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef MATH3D_H
-#define MATH3D_H
-
-// #include <cstdlib>
-// #include <cuda_runtime.h>
-#include "m3Vector.h"
-
-#endif
--- README.md
+++
@@ -1,1 +0,0 @@
-# Implementation of Smoothed particle Hydrodynamics, and CUDA
